97818608da056171c0132132c9843c0799c38f4f
Brian Lee <blee1126@gmail.com>
2022-04-05 19:46:53 -0500

    2nd commit

---
+++ waterpurveyor/src/waterpurveyor/WaterPurveyor.java
@@ -0,0 +1,7 @@
+package waterpurveyor;
+
+public class WaterPurveyor {
+	public static void main(String[] args) {
+		System.out.println("Hello! Welcome to The Water Purveyor!  Dealer of the finest waters known to man!");
+	}
+}
